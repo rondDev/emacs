9b07e1faf50cc785bdf97f40eec64df3b8ee9522
rondDev <contact@rond.cc>
2025-11-17 10:55:16 +0100

    feat: add early-init

---
+++ early-init.el
@@ -0,0 +1,37 @@
+;; improves startup speed when using an alternative package manager
+(setq package-enable-at-startup nil)
+
+
+;; ** Disable Tool Bar, Menu Bar, and Scroll Bar
+;; doing this here reduces init time by ~0.2 seconds for me
+;; disabling `tool-bar-mode' in normal init file takes ~0.1s
+;; https://github.com/raxod502/radian/issues/180
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(horizontal-scroll-bars) default-frame-alist)
+(let ((gap (or (getenv "WM_GAP") "15")))
+  (push (cons 'internal-border-width (string-to-number gap)) default-frame-alist))
+
+;; unfortunately Emacs has no way to exclude text from being transparent
+;; active and inactive alpha
+(unless (string= (getenv "XDG_SESSION_TYPE") "wayland")
+  (push '(alpha . (85 . 85)) default-frame-alist))
+
+;; no titlebar
+;; added in a patch; see my emacs.nix overlay
+(when (eq system-type 'darwin)
+  (push '(undecorated-round . t) default-frame-alist))
+
+;; * Prevent Default Mode Line from Showing
+;; https://github.com/hlissner/doom-emacs/blob/7460e9e7989c9b219879073690e6f43ac535d274/modules/ui/modeline/config.el#L16
+;; doesn't actually need to be set this early but it still makes sense to put it
+;; here
+;; TODO: uncomment
+; (unless after-init-time
+;   ;; prevent flash of unstyled modeline at startup
+;   (setq-default mode-line-format nil))
+
+;; * Silence lexical binding warning
+;; don't show warning buffer for; tons of packages are missing it
+(setq warning-suppress-types '((files)))
