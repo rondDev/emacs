a80bcb71418a5f492e2efcb888f9c8678cc90bca
rondDev <contact@rond.cc>
2025-11-19 07:09:02 +0100

    fix: remove hl-todo

--- modules/ui/packages.el
+++ modules/ui/packages.el
@@ -19,39 +19,6 @@
 
 (package! eldoc-box)
 
-;; (package! flycheck-hl-todo
-;;   :config
-;;   (flycheck-hl-todo-setup)) ; https://github.com/alvarogonzalezsotillo/flycheck-hl-todo
-
-(package! hl-todo
-  :init
- (add-hook 'emacs-startup-hook #'global-hl-todo-mode)
- :config
- (setq hl-todo-highlight-punctuation ":"
-   hl-todo-keyword-faces
-   '(;; For reminders to change or add something at a later date.
-     ("TODO" warning bold
-         ;; For code (or code paths) that are broken, unimplemented, or slow,
-         ;; and may become bigger problems later.
-         ("FIXME" error bold)
-         ;; For code that needs to be revisited later, either to upstream it,
-         ;; improve it, or address non-critical issues.
-         ("REVIEW" font-lock-keyword-face bold)
-         ;; For code smells where questionable practices are used
-         ;; intentionally, and/or is likely to break in a future update.
-         ("HACK" font-lock-constant-face bold)
-         ;; For sections of code that just gotta go, and will be gone soon.
-         ;; Specifically, this means the code is deprecated, not necessarily
-         ;; the feature it enables.
-         ("DEPRECATED" font-lock-doc-face bold)
-         ;; Extra keywords commonly found in the wild, whose meaning may vary
-         ;; from project to project.
-         ("NOTE" success bold)
-         ("BUG" error bold)
-       ("XXX" font-lock-constant-face bold)))))
-  
-                                          ; https://github.com/tarsius/hl-todo
-
 (package! magit-todos
   :after magit
   :config
