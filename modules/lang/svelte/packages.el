d028a1fd39de0ab51bd5694ec69f107bee18daab
rondDev <contact@rond.cc>
2025-11-19 11:36:46 +0100

    feat: add svelte-mode and web-mode

--- modules/lang/svelte/packages.el
+++ modules/lang/svelte/packages.el
@@ -1,6 +1,17 @@
+(package! web-mode)
+  
+
 (package! typescript-mode
   :defer 10)
 
-(package! svelte-ts-mode
-  :after eglot
-  :ensure (:host github :repo "leafOfTree/svelte-ts-mode"))
+(package! svelte-mode
+  ;; :after eglot
+  :mode "\\.svelte\\'"
+  :config
+  ;; (add-hook 'svelte-mode-hook #'eglot-ensure)
+  ;; (add-hook 'svelte-mode-hook #'eglot-booster-mode)
+  :ensure (:host github :repo "leafOfTree/svelte-mode"))
+
+;; (package! svelte-ts-mode
+;;   :mode "\\.svelte\\'"
+;;   :ensure (:host github :repo "leafOfTree/svelte-ts-mode"))
