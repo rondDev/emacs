8c85a9f26124157af92f3ce572ec796f9b901a95
rondDev <contact@rond.cc>
2025-11-24 20:11:08 +0100

    fix: evil-collection config

--- modules/default/packages.el
+++ modules/default/packages.el
@@ -23,7 +23,14 @@
   (editorconfig-mode 1))
 
 
-(package! evil-collection)
+(package! evil-collection
+  :custom
+  (evil-collection-magit-use-z-for-folds t
+                                         evil-collection-magit-use-y-for-yank t
+                                         evil-collection-setup-minibuffer t)
+  :config
+  (evil-collection-init)
+  (setq evil-emacs-state-modes (delq 'ibuffer-mode evil-emacs-state-modes)))
 
 (package! evil-goggles)
 
