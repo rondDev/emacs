1c67bff8b74ac38b511ac9e31f1a78667b8b61f7
rondDev <contact@rond.cc>
2025-11-24 20:30:57 +0100

    fix: benchmark-init

--- init.el
+++ init.el
@@ -113,9 +113,64 @@
             (set-frame-parameter (selected-frame) 'alpha 100) "100 for fully opaque"
             (set-frame-parameter (selected-frame) 'background-alpha 100)))
 
+
+
+                        ;;; Tramp
+;; TRAMP performance settings
+(setq tramp-use-ssh-controlmaster-options nil) ; Use your SSH config instead
+(setq tramp-default-method "scp")             ; Force SSH method
+(setq password-cache-expiry 3600)             ; Cache passwords longer
+
+;; For faster connection establishment
+(setq tramp-connection-timeout 10)
+(setq tramp-verbose 3) ; Reduce if too verbose, increase for debugging
+(setq remote-file-name-inhibit-locks t
+      tramp-use-scp-direct-remote-copying t
+      remote-file-name-inhibit-auto-save-visited t)
+(setq tramp-copy-size-limit (* 8 1024 1024) ;; 8MB
+      tramp-verbose 2)
+
+(connection-local-set-profile-variables
+ 'remote-direct-async-process
+ '((tramp-direct-async-process . t)))
+
+(connection-local-set-profiles
+ '(:application tramp :protocol "scp")
+ 'remote-direct-async-process)
+
+(setq magit-tramp-pipe-stty-settings 'pty)
+
+(with-eval-after-load 'tramp
+  (with-eval-after-load 'compile
+    (remove-hook 'compilation-mode-hook #'tramp-compile-disable-ssh-controlmaster-options)))
+
+
+(setq shell-file-name (executable-find
+                       "fish"))
+
+(setq-default vterm-shell
+              "/usr/bin/fish") (setq-default explicit-shell-file-name)
+(setq vterm-eval-cmds '(("find-file" find-file)
+                        ("message" message)
+                        ("vterm-clear-scrollback" vterm-clear-scrollback)
+                        ("dired" dired)
+                        ("ediff-files" ediff-files)))
+"/usr/bin/fish"
+(setq vc-ignore-dir-regexp
+      (format "\\(%s\\)\\|\\(%s\\)"
+              vc-ignore-dir-regexp
+              tramp-file-name-regexp))
 ;; (mapc 'load (file-expand-wildcards (concat user-emacs-directory "modules/*/*.el")))
 ;; TODO: Improve this loading, it's really messy
 (load (expand-file-name "lisp/elpaca-setup.el" user-emacs-directory))
+
+;; No real effect on startup time
+(use-package benchmark-init
+  :ensure t
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 (load (expand-file-name "modules/initial-packages.el" user-emacs-directory))
 (load (expand-file-name "modules/default/keybindings.el" user-emacs-directory))
 (load (expand-file-name "modules/default/packages.el" user-emacs-directory))
