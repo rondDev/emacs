e787b915520a0cf2b4faa5ca903dfb07535a8f70
rondDev <contact@rond.cc>
2025-11-24 20:11:47 +0100

    fix: formatting and move evil-collection config to package initialization

--- modules/git/packages.el
+++ modules/git/packages.el
@@ -4,18 +4,20 @@
   :defer 12)
 (package! magit
   :general (:states '(normal visual motion)
-            :keymaps '(override magit-mode-map magit-status-mode)
-            "h" 'evil-backward-char
-            "j" 'evil-next-visual-line
-            "k" 'evil-previous-line
-            "l" 'evil-forward-char)
-                      
-                      
+                    :keymaps '(override magit-mode-map magit-status-mode)
+                    "h" 'evil-backward-char
+                    "j" 'evil-next-visual-line
+                    "k" 'evil-previous-line
+                    "l" 'evil-forward-char)
+  
+  
   :config
- (add-hook 'git-commit-mode-hook 'evil-insert-state)
- (evil-set-initial-state 'git-commit-mode 'insert)
- (evil-set-initial-state 'magit-status-mode 'normal)
- (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
+  (add-hook 'git-commit-mode-hook 'evil-insert-state)
+  (evil-set-initial-state 'git-commit-mode 'insert)
+  (evil-set-initial-state 'magit-status-mode 'normal)
+  (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1
+        evil-collection-magit-use-z-for-folds t
+        evil-collection-magit-use-y-for-yank t))
 
 (package! tramp
   :ensure nil
