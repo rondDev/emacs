2f848297775d438c1fee445b56e6e848c828ec6b
rondDev <contact@rond.cc>
2025-11-24 20:16:49 +0100

    fix: formatting

--- modules/org/packages.el
+++ modules/org/packages.el
@@ -2,11 +2,11 @@
 (package! org
   :defer 4
   :config
- (add-hook 'after-init-hook 'org-mode)
- (add-hook 'after-init-hook #'org-indent-mode)
- (add-hook 'after-init-hook '(setq org-agenda-files (seq-filter (lambda(x) (not (string-match "\\/.#" x)))
-                                                     (directory-files-recursively "~/org" "\\.org$"))
-                              org-directory "~/org")))
+  (add-hook 'after-init-hook 'org-mode)
+  (add-hook 'after-init-hook #'org-indent-mode)
+  (add-hook 'after-init-hook '(setq org-agenda-files (seq-filter (lambda(x) (not (string-match "\\/.#" x)))
+                                                                 (directory-files-recursively "~/org" "\\.org$"))
+                                    org-directory "~/org")))
 
 (package! org-super-agenda
   :defer t)
@@ -23,7 +23,7 @@
          ;; Dailies
          ("C-c n j" . org-roam-dailies-capture-today))
   :config
-   ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
