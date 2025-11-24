12a26fb2d512c8f580465e0625c609c193c02399
rondDev <contact@rond.cc>
2025-11-24 20:19:50 +0100

    fix: add themes

---
+++ themes/other/packages.el
@@ -0,0 +1,11 @@
+(package! cyberpunk-theme
+  :defer 4)
+(package! kaolin-themes
+  :defer 4)
+(package! cherry-blossom-theme
+  :defer 4)
+
+(package! sakura-theme
+  :config
+  (load-theme 'sakura t))
+
