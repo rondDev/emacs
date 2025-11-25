44401860b72c9122e5f0dd667c9a58c3bf751351
rondDev <contact@rond.cc>
2025-11-25 21:50:22 +0100

    fix: only remove shell argument if fish not executable

--- modules/default/packages.el
+++ modules/default/packages.el
@@ -47,7 +47,8 @@
 
 (package! exec-path-from-shell
   :init
-  (setq exec-path-from-shell-arguments "")
+  (when (file-executable-p "/usr/sbin/fish")
+    (setq exec-path-from-shell-arguments ""))
   :config
   (add-hook 'emacs-startup-hook #'exec-path-from-shell-initialize))
 
