f815ed13881dcff1922b1e5bfbd1657ebaccdfce
rondDev <contact@rond.cc>
2025-11-25 19:59:50 +0100

    fix: update lang config

--- modules/lang/config.el
+++ modules/lang/config.el
@@ -5,48 +5,15 @@
 ;; Author:  <rond@oizys>
 ;; Keywords: 
 
-;;;###autoload
-(defun lsp-booster--advice-json-parse (old-fn &rest args)
-  "Try to parse bytecode instead of json."
-  (or
-   (when (equal (following-char) ?#)
-     (let ((bytecode (read (current-buffer))))
-       (when (byte-code-function-p bytecode)
-         (funcall bytecode))))
-   (apply old-fn args)))
-(advice-add (if (progn (require 'json)
-                       (fboundp 'json-parse-buffer))
-                'json-parse-buffer
-              'json-read)
-            :around
-            #'lsp-booster--advice-json-parse)
-
-;;;###autoload
-(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-  "Prepend emacs-lsp-booster command to lsp CMD."
-  (let ((orig-result (funcall old-fn cmd test?)))
-    (if (and (not test?)                             ;; for check lsp-server-present?
-             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
-             lsp-use-plists
-             (not (functionp 'json-rpc-connection))  ;; native json-rpc
-             (executable-find "emacs-lsp-booster"))
-        (progn
-          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
-            (setcar orig-result command-from-exec-path))
-          (message "Using emacs-lsp-booster for %s!" orig-result)
-          (cons "emacs-lsp-booster" orig-result))
-      orig-result)))
-(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
-
 (after! apheleia
         ;; NOTE: THERE HAS TO BE A BETTER WAY TO DO THIS
-        (after! svelte-mode
-                ;; (setf apheleia-formatters
-                ;;       (assq-delete-all 'prettier-typescript apheleia-formatters))
-                (push '(denofmt-svelte . ("deno" "fmt" "--unstable-component" "--ext" "svelte" "-"))
-                      apheleia-formatters))
+        (setf apheleia-formatters
+              (assq-delete-all 'prettier-svelte apheleia-formatters))
+        (push '(denofmt-svelte . ("deno" "fmt" "--unstable-component" "--ext" "svelte" "-"))
+              apheleia-formatters)
         ;; (setf (alist-get 'prettier-typescript apheleia-formatters) '("deno" "fmt" "-"))
-        (setf (alist-get 'prettier-typescript apheleia-formatters) '("deno" "fmt" "--unstable-component" "--ext" "svelte" "-")))
+        (setf (alist-get 'svelte-mode apheleia-mode-alist) 'denofmt-svelte)
+        (setf (alist-get 'prettier-svelte apheleia-formatters) '("deno" "fmt" "--unstable-component" "--ext" "svelte" "-")))
 
 (after! corfu
         (setq corfu-auto t
