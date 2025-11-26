90dbcac74d4831f47b95885b6979925ad7e34917
rondDev <contact@rond.cc>
2025-11-26 17:35:51 +0100

    fix: moved ansi color fix to rond-util

--- lisp/rond-util.el
+++ lisp/rond-util.el
@@ -126,4 +126,13 @@
       (setq compile-command command-text)
       (compile compile-command))))
 
+
+;; NOTE: Fixes ansi colors in compilation mode
+(ignore-errors
+  (require 'ansi-color)
+  (defun my-colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
+
 (provide 'rond/util)
