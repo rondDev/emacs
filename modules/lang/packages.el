cbf8eee4654412789d10857c7fbf09adee94386b
rondDev <contact@rond.cc>
2025-11-26 11:54:22 +0100

    fix: dont defer yasnippet

--- modules/lang/packages.el
+++ modules/lang/packages.el
@@ -57,8 +57,7 @@
 (package! tree-sitter-langs
   :defer t)
 
-(package! yasnippet
-  :defer t)
+(package! yasnippet)
 
 (mapc 'load (file-expand-wildcards (concat user-emacs-directory "modules/lang/*/*.el")))
 (load (expand-file-name "modules/lang/config.el" user-emacs-directory))
