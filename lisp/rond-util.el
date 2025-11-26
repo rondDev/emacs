c5ecb5fef98eae87429b8ed0283ef1b6576caa7e
rondDev <contact@rond.cc>
2025-11-26 17:43:16 +0100

    feat: add "rename-current-buffer-file"

--- lisp/rond-util.el
+++ lisp/rond-util.el
@@ -126,6 +126,33 @@
       (setq compile-command command-text)
       (compile compile-command))))
 
+;;;###autoload
+(defun spacemacs/rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let* ((name (buffer-name))
+         (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let* ((dir (file-name-directory filename))
+             (new-name (read-file-name "New name: " dir)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (let ((dir (file-name-directory new-name)))
+                 (when (and (not (file-exists-p dir)) (yes-or-no-p (format "Create directory '%s'?" dir)))
+                   (make-directory dir t)))
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (when (fboundp 'recentf-add-file)
+                 (recentf-add-file new-name)
+                 (recentf-remove-if-non-kept filename))
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
+
+
+
 
 ;; NOTE: Fixes ansi colors in compilation mode
 (ignore-errors
