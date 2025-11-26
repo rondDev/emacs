892d7356b8513955dea64100846aef72bd0e1d9d
rondDev <contact@rond.cc>
2025-11-26 18:41:12 +0100

    fix: package initialization on package-install

--- init.el
+++ init.el
@@ -152,7 +152,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(advice-add 'package-install :before '(package-initialize))
+(advice-add 'package-install :before (package-initialize))
 
 
 (package! welcome-dashboard
