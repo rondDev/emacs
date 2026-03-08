23e22ada97acf568c89c9cae4e3f3634a1779b27
rondDev <contact@rond.cc>
2026-03-08 07:17:05 +0100

    fix: elpaca update

--- lisp/elpaca-setup.el
+++ lisp/elpaca-setup.el
@@ -1,16 +1,15 @@
 ;;; -*- lexical-binding: t -*-
 ;; HACK: Building from master necessitates this
-(defvar elpaca-core-date 20250814)
 
-(defvar elpaca-installer-version 0.11)
+(defvar elpaca-installer-version 0.12)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+                              :build (:not elpaca-activate)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
        (default-directory repo))
@@ -40,8 +39,7 @@
     (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
-(setq elpaca-queue-limit 30)
-(elpaca-wait)
+;; (setq elpaca-queue-limit 30)
 
 ;; Uncomment for systems which cannot create symlinks:
 ;; (elpaca-no-symlink-mode)
@@ -59,3 +57,10 @@
   (setq use-package-always-ensure t))
 
 (setq elpaca-menu-org-make-manual nil)
+
+(if debug-on-error
+    (setq use-package-verbose t
+          use-package-expand-minimally nil
+          use-package-compute-statistics t)
+  (setq use-package-verbose nil
+        use-package-expand-minimally t))
