8c960c0b809fc3fe0a75841d4dbb503bf8ee6822
rondDev <contact@rond.cc>
2026-03-09 16:18:20 +0100

    fix: housekeeping git packages

---
+++ modules/git/config.el
@@ -0,0 +1,22 @@
+(after! magit
+        (def!
+          :states '(normal visual motion)
+          :keymaps '(override magit-mode-map magit-status-mode)
+          "h" 'evil-backward-char
+          "j" 'evil-next-visual-line
+          "k" 'evil-previous-line
+          "l" 'evil-forward-char)
+        (add-hook 'git-commit-mode-hook 'evil-insert-state)
+        (evil-set-initial-state 'git-commit-mode 'insert)
+        (evil-set-initial-state 'magit-status-mode 'normal)
+        (setq-default magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1
+                      evil-collection-magit-use-z-for-folds t
+                      evil-collection-magit-section-use-z-for-folds t
+                      evil-collection-magit-use-y-for-yank t)
+        (setq magit-clone-default-directory "~/code/"
+              magit-diff-visit-previous-blob nil)
+        (transient-bind-q-to-quit))
+
+(after! tramp
+        (setq remote-file-name-inhibit-locks t
+              remote-file-name-inhibit-auto-save-visited t))
--- modules/git/packages.el
+++ modules/git/packages.el
@@ -8,29 +8,8 @@
 
 (package! magit
   :defer t
-  :after general
-  :general
-  (:states '(normal visual motion)
-           :keymaps '(override magit-mode-map magit-status-mode)
-           "h" 'evil-backward-char
-           "j" 'evil-next-visual-line
-           "k" 'evil-previous-line
-           "l" 'evil-forward-char)
-  :config
-  (add-hook 'git-commit-mode-hook 'evil-insert-state)
-  (evil-set-initial-state 'git-commit-mode 'insert)
-  (evil-set-initial-state 'magit-status-mode 'normal)
-  (setq-default magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1
-                evil-collection-magit-use-z-for-folds t
-                evil-collection-magit-section-use-z-for-folds t
-                evil-collection-magit-use-y-for-yank t)
-  (setq magit-clone-default-directory "~/code/"
-        magit-diff-visit-previous-blob nil)
-  (transient-bind-q-to-quit))
+  :after general)
 
 (package! tramp
   :ensure nil
-  :defer t
-  :config
-  (setq remote-file-name-inhibit-locks t
-        remote-file-name-inhibit-auto-save-visited t))
+  :defer t)
