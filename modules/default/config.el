4f82151fd9919dd38a2b050132a6add1d5b83abb
rondDev <contact@rond.cc>
2026-03-09 15:00:20 +0100

    fix: housekeeping for default packages

---
+++ modules/default/config.el
@@ -0,0 +1,111 @@
+(after! ace-window
+        (setopt aw-dispatch-always t)
+        ;; NOTE: which-key might not work here, unsure
+        (general-spc
+          "j" #'ace-window
+          "jx" '(:ignore t :wk "delete window")
+          "jm" '(:ignore t :wk "swap windows")
+          "jM" '(:ignore t :wk "move window")
+          "jc" '(:ignore t :wk "copy window")
+          "jj" '(:ignore t :wk "select buffer")
+          "jn" '(:ignore t :wk "select previous buffer")
+          "ju" '(:ignore t :wk "select buffer in other window")
+          "jc" '(:ignore t :wk "split fairly")
+          "jv" '(:ignore t :wk "split vertically")
+          "jb" '(:ignore t :wk "split horizontally")
+          "jo" '(:ignore t :wk "maximize current window")
+          "j?" '(:ignore t :wk "show bindings"))
+        (setq aw-keys '(?a ?s ?d ?f ?g ?h ?k ?l
+                           aw-dispatch-always t)))
+
+(after! async
+        (autoload 'dired-async-mode "dired-async.el" nil t)
+        ;; make dired async
+        (dired-async-mode 1)
+        (async-bytecomp-package-mode 1))
+
+(after! autorevert
+        (setopt auto-revert-interval 0.01 "Instantaneously revert")
+        (global-auto-revert-mode t))
+
+(after! auto-sudoedit
+        (auto-sudoedit-mode 1)) ; automatically open with sudo
+
+(after! compile
+        (defun +compilation-colorize ()
+          "Colorize from `compilation-filter-start' to `point'."
+          (require 'ansi-color)
+          (let ((inhibit-read-only t))
+            (ansi-color-apply-on-region (point-min) (point-max))))
+        (add-hook 'compilation-filter-hook #'+compilation-colorize))
+
+(after! dired
+        (setopt dired-mouse-drag-files t)
+        (setopt dired-listing-switches "-alh" "Human friendly file sizes.")
+        (setopt dired-kill-when-opening-new-dired-buffer t)
+        (setopt dired-omit-files "\\(?:\\.+[^z-a]*\\)")
+        (add-hook 'dired-mode-hook 'dired-omit-mode))
+
+(after! editorconfig
+        (editorconfig-mode 1))
+
+(after! evil-collection
+        (setq evil-collection-magit-use-z-for-folds t
+              evil-collection-magit-use-y-for-yank t)
+        (setq evil-emacs-state-modes (delq 'ibuffer-mode evil-emacs-state-modes))
+        (evil-collection-init))
+
+(after! evil-nerd-commenter
+        (evilnc-default-hotkeys))
+
+(after! evil-surround
+        (global-evil-surround-mode 1))
+
+(after! evil-quickscope
+        (evil-quickscope-always-mode))
+
+(after! exec-path-from-shell
+        (add-hook 'emacs-startup-hook #'exec-path-from-shell-initialize))
+
+(after! orderless
+        (setq completion-styles '(orderless basic))
+        (setq completion-category-overrides '((file (styles partial-completion))))
+        (setq completion-category-defaults nil)
+        (setq completion-pcm-leading-wildcard t))
+
+(after! persp-projectile
+        (setq persp-mode-prefix-key "SPC.")
+        (persp-mode))
+
+(after! perspective
+        (defun persp-new (name)
+          "Return a perspective named NAME, or create a new one if missing.
+The new perspective will start with only an `initial-major-mode'
+buffer called \"*scratch* (NAME)\"."
+          (or (gethash name (perspectives-hash))
+              (make-persp :name name
+                (persp-reset-windows)))))
+
+(after! projectile-git-autofetch
+        (setopt projectile-git-autofetch-notify nil)
+        (setopt projectile-git-autofetch-interval 60)
+        (setopt projectile-git-autofetch-fetch-args '("--no-progress" "--prune" "--prune-tags")))
+
+(after! smartparens
+        (require 'smartparens-config)) ;; add `smartparens-mode` to these hooks
+
+(after! undo-fu
+        (setq undo-limit 67108864) ; 64mb.
+        (setq undo-strong-limit 100663296) ; 96mb.
+        (setq undo-outer-limit 1006632960)) ; 960mb.
+
+(after! vertico
+        (setq vertico-cycle t)
+        (setq vertico-count 20)
+        (setq vertico-resize nil)
+        (vertico-mode)
+        (setq ido-mode nil))
+
+(after! wakatime-mode
+        (when (file-executable-p "/usr/sbin/wakatime")
+          (global-wakatime-mode)))
--- modules/default/packages.el
+++ modules/default/packages.el
@@ -1,35 +1,8 @@
 ;;; -*- lexical-binding: t -*-
 (package! ace-window
-  :after general
-  ;; NOTE: which-key might not work here, unsure
-  :general (general-spc
-             "j" #'ace-window
-             "jx" '(:ignore t :wk "delete window")
-             "jm" '(:ignore t :wk "swap windows")
-             "jM" '(:ignore t :wk "move window")
-             "jc" '(:ignore t :wk "copy window")
-             "jj" '(:ignore t :wk "select buffer")
-             "jn" '(:ignore t :wk "select previous buffer")
-             "ju" '(:ignore t :wk "select buffer in other window")
-             "jc" '(:ignore t :wk "split fairly")
-             "jv" '(:ignore t :wk "split vertically")
-             "jb" '(:ignore t :wk "split horizontally")
-             "jo" '(:ignore t :wk "maximize current window")
-             "j?" '(:ignore t :wk "show bindings"))
+  :after general)
 
-  
-  :custom
-  (aw-dispatch-always t)
-  :config
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?k ?l
-                     aw-dispatch-always t)))
-
-(package! async
-  :config
-  (autoload 'dired-async-mode "dired-async.el" nil t)
-  ;; make dired async
-  (dired-async-mode 1)
-  (async-bytecomp-package-mode 1))
+(package! async)
 
 (package! auth-source
   :ensure nil
@@ -38,52 +11,27 @@
 
 (package! autorevert
   :ensure nil
-  :defer 2
-  :custom
-  (auto-revert-interval 0.01 "Instantaneously revert")
-  :config
-  (global-auto-revert-mode t))
+  :defer 2)
 
 (package! auto-sudoedit
-  :defer 3
-  :config
-  (auto-sudoedit-mode 1)) ; automatically open with sudo
+  :defer 3)
 
 (package! avy)
 
 (package! complile
   :ensure nil
-  :commands (compile recompile)
-  :config
-  (defun +compilation-colorize ()
-    "Colorize from `compilation-filter-start' to `point'."
-    (require 'ansi-color)
-    (let ((inhibit-read-only t))
-      (ansi-color-apply-on-region (point-min) (point-max))))
-  (add-hook 'compilation-filter-hook #'+compilation-colorize))
+  :commands (compile recompile))
 
 (package! consult)
 
-
 (package! dired
   :ensure nil
-  :commands (dired)
-  :custom
-  (dired-mouse-drag-files t)
-  (dired-listing-switches "-alh" "Human friendly file sizes.")
-  (dired-kill-when-opening-new-dired-buffer t)
-  (dired-omit-files "\\(?:\\.+[^z-a]*\\)")
-  :hook (dired-mode-hook . dired-omit-mode))
+  :commands (dired))
 
-(package! editorconfig
-  :defer 6
-  :config
-  (editorconfig-mode 1))
+(package! editorconfig)
 
 (package! emp
-  :ensure (emp :host github :repo "progfolio/emp")
-  :config
-  :general)
+  :ensure (emp :host github :repo "progfolio/emp"))
 ;; (+general-global-application
 ;;  "v"  '(:ignore t :which-key "video/audio")
 ;;  "vQ" 'emp-kill
@@ -104,41 +52,26 @@
   :after (evil anzu))
 
 (package! evil-collection
-  :after (evil)
-  ;; :init (setq evil-collection-setup-minibuffer t)
-  :config
-  (setq evil-collection-magit-use-z-for-folds t
-        evil-collection-magit-use-y-for-yank t)
-  (setq evil-emacs-state-modes (delq 'ibuffer-mode evil-emacs-state-modes))
-  (evil-collection-init))
+  :after (evil))
 
 (package! evil-goggles
   :after (evil))
 
 (package! evil-nerd-commenter
-  :after (evil)
-  :config
-  (evilnc-default-hotkeys))
+  :after (evil))
 
 (package! evil-surround
-  :after (evil)
-  :config
-  (global-evil-surround-mode 1))
+  :after (evil))
 
 ;; (package! evil-vimish-fold
 ;;   :config
 ;;   (add-hook 'prog-mode-hook 'evil-vimish-fold-mode)
 ;;   (add-hook 'text-mode-hook 'evil-vimish-fold-mode))
 
-
 (package! evil-quickscope
-  :config
-  (evil-quickscope-always-mode))
+  :after (evil))
 
-(package! exec-path-from-shell
-  :init
-  :config
-  (add-hook 'emacs-startup-hook #'exec-path-from-shell-initialize))
+(package! exec-path-from-shell)
 
 ;; BUG: find-function--search-by-expanding-macros: Invalid escape char syntax: \A not followed by -
 ;; NOTE: Bug occurs after startup, seems to go away after opening a project
@@ -151,12 +84,7 @@
 (package! on
   :defer 3)
 
-(package! orderless
-  :config
-  (setq completion-styles '(orderless basic))
-  (setq completion-category-overrides '((file (styles partial-completion))))
-  (setq completion-category-defaults nil)
-  (setq completion-pcm-leading-wildcard t))
+(package! orderless)
 
 (package! persistent-scratch
   :init
@@ -166,20 +94,9 @@
 ;; https://github.com/karthink/popper
 
 (package! persp-projectile
-  :after (projectile)
-  :config
-  (setq persp-mode-prefix-key "SPC.")
-  (persp-mode))
+  :after (projectile))
 
-(package! perspective
-  :config
-  (defun persp-new (name)
-    "Return a perspective named NAME, or create a new one if missing.
-The new perspective will start with only an `initial-major-mode'
-buffer called \"*scratch* (NAME)\"."
-    (or (gethash name (perspectives-hash))
-        (make-persp :name name
-          (persp-reset-windows)))))
+(package! perspective)
 
 (package! projectile
   :after (general)
@@ -194,31 +111,19 @@ buffer called \"*scratch* (NAME)\"."
 (package! projectile-git-autofetch
   :after projectile
   :init
-  (add-hook 'emacs-startup-hook #'projectile-git-autofetch-setup)
-  :config
-  (setopt projectile-git-autofetch-notify nil)
-  (setopt projectile-git-autofetch-interval 60)
-  (setopt projectile-git-autofetch-fetch-args '("--no-progress" "--prune" "--prune-tags")))
-
-
+  (add-hook 'emacs-startup-hook #'projectile-git-autofetch-setup))
 
 (package! rg
   :defer 20)
 
 (package! smartparens
   :defer 1
-  :hook (prog-mode text-mode markdown-mode)
-  :config
-  (require 'smartparens-config)) ;; add `smartparens-mode` to these hooks
+  :hook (prog-mode text-mode markdown-mode))
 
 (package! transient
   :defer t)
 
-(package! undo-fu
-  :config
-  (setq undo-limit 67108864) ; 64mb.
-  (setq undo-strong-limit 100663296) ; 96mb.
-  (setq undo-outer-limit 1006632960)) ; 960mb. )
+(package! undo-fu)
 
 (package! undo-fu-session
   :hook (text-mode prog-mode)
@@ -230,16 +135,7 @@ buffer called \"*scratch* (NAME)\"."
   :custom
   (vc-follow-symlinks t))
 
-
-(package! vertico
-  :config
-  (setq vertico-cycle t)
-  (setq vertico-count 20)
-  (setq vertico-resize nil)
-  (vertico-mode)
-  (setq ido-mode nil))
-
-
+(package! vertico)
 
 (package! vterm
   :ensure (vterm :post-build
@@ -269,9 +165,4 @@ buffer called \"*scratch* (NAME)\"."
   :config
   (evil-set-initial-state 'vterm-mode 'insert))
 
-
-
-(package! wakatime-mode
-  :config
-  (when (file-executable-p "/usr/sbin/wakatime")
-    (global-wakatime-mode)))
+(package! wakatime-mode)
