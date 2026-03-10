917ad6f184288a0fc8d38637a60f9ea8df0fbf17
rondDev <contact@rond.cc>
2026-03-10 14:46:27 +0100

    fix: svelte mode

--- modules/lang/svelte/config.el
+++ modules/lang/svelte/config.el
@@ -1,43 +1,4 @@
 ;;; -*- lexical-binding: t -*-
-;; (after! svelte-ts-mode
-;;   (add-to-list 'eglot-server-programs '(svelte-ts-mode . ("svelteserver" "--stdio")))
-;;   (add-hook 'svelte-ts-mode-hook #'web-mode))
-
-;; (custom-set-faces
-;;  '(flyover-error
-;;    ((t :background "#453246"
-;;        :foreground "#ea8faa"
-;;        :height 0.9
-;;        :weight normal)))
-
-;;  '(flyover-warning
-;;    ((t :background "#331100"
-;;        :foreground "#DCA561"
-;;        :height 0.9
-;;        :weight normal)))
-
-;;  '(flyover-info
-;;    ((t :background "#374243"
-;;        :foreground "#a8e3a9"
-;;        :height 0.9
-;;        :weight normal))))
-
-;; (after! flyover
-;;  (add-hook 'flycheck-mode-hook #'flyover-mode)
-;;  (setq flyover-virtual-line-type 'curved-arrow
-;;        flyover-percent-darker 60
-;;        flyover-text-tint-percent 100
-;;        flyover-text-tint 'lighter
-;;        flyover-show-at-eol nil
-;;        flyover-wrap-messages t
-;;        flyover-max-lines 110
-;;        flyover-debounce-interval 2.0
-;;        flyover-background-lightness 20
-;;        flyover-virtual-line-icon nil))
-
-;; (defun init-mode-svelte ()
-;;   (add-to-list 'auto-mode-alist '("\\.svelte\\'" . svelte-mode))
-;;   (add-to-list 'eglot-server-programs '(svelte-mode . ("typescript-language-server" "--stdio"))))
 
 (rassq-delete-all 'js-mode auto-mode-alist)
 ;; (defun init-mode-svelte ())
@@ -56,9 +17,8 @@
 
 (add-hook 'find-file-hook #'rond/file-mode-hook)
 
-;; (after! eglot
-;;   (init-mode-svelte))
+(after! eglot
+        (add-to-list 'eglot-server-programs
+                     '(svelte-mode . ("rass" "svelte"))))
 
-
-;; (add-hook 'svelte-ts-mode-hook #'eglot-ensure)
-;; (add-hook 'svelte-ts-mode-hook #'flycheck-mode)
+(add-hook 'svelte-mode-hook #'eglot-ensure)
