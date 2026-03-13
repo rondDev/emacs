1fec5c9bc0d818b57fe55e85b285874e29de1c72
rondDev <contact@rond.cc>
2026-03-13 20:12:58 +0100

    fix(lang): typescript

--- modules/lang/typescript/config.el
+++ modules/lang/typescript/config.el
@@ -1,7 +1,11 @@
+(add-hook 'typescript-ts-mode-hook #'eglot-ensure)
+(add-hook 'tsx-ts-mode-hook #'eglot-ensure)
+(add-hook 'js-ts-mode-hook #'eglot-ensure)
 
 (after! eglot
         (rassq-delete-all 'typescript-ts-mode eglot-server-programs)
         (add-to-list 'eglot-server-programs
-                     '(typescript-ts-mode . ("rass" "ts"))))
-
-(add-hook 'typescript-ts-mode-hook #'eglot-ensure)
+                     '((typescript-ts-mode js-mode) . ("rass" "ts"))))
+;; (add-to-list 'eglot-server-programs
+;;              '((typescript-ts-mode tsx-ts-mode js-ts-mode)
+;;                . (eglot-deno "deno" "lsp")))
