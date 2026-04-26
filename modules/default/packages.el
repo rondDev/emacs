6b2b9e8b3d709e34c735c0bfae3fd07b2d778d3c
rondDev <contact@rond.cc>
2026-04-27 00:09:14 +0200

    fix: projectile stuff

--- modules/default/packages.el
+++ modules/default/packages.el
@@ -62,26 +62,6 @@
 ;; NOTE: Could consider adding popper.
 ;; https://github.com/karthink/popper
 
-(package! persp-projectile
-  :after (projectile))
-
-(package! perspective)
-
-(package! projectile
-  :after (general)
-  :init
-  (setq projectile-project-search-path '("~/code" "~/.config" ("~/projects" . 2))
-        projectile-enable-caching t
-        projectile-sort-order 'recently-active)
-  (projectile-mode +1)
-  (add-hook 'elpaca-after-init-hook #'projectile-discover-projects-in-search-path)
-  (add-hook 'elpaca-after-init-hook #'projectile-load-known-projects))
-
-(package! projectile-git-autofetch
-  :after projectile
-  :init
-  (after! projectile
-          (add-hook 'emacs-startup-hook #'projectile-git-autofetch-setup)))
 
 (package! transient
   :defer t)
