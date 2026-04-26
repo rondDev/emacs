4209f45488ddbe91b29eebe4788ace30381aa469
rondDev <contact@rond.cc>
2026-04-26 23:13:38 +0200

    fix: ruby-mode

--- modules/lang/ruby/config.el
+++ modules/lang/ruby/config.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t; -*-
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(rassq-delete-all 'ruby-ts-mode auto-mode-alist)
 (after! eglot
         (add-to-list 'eglot-server-programs
-                     '(ruby-ts-mode . ("ruby-lsp")))
-        (add-hook 'ruby-ts-mode #'eglot-ensure))
+                     '(ruby-mode . ("ruby-lsp"))))
+
+(add-hook 'ruby-mode #'eglot-ensure)
