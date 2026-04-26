7ede14d61cc4d4f29d2ece9e98f12e8123e276a0
rondDev <contact@rond.cc>
2026-04-27 00:08:57 +0200

    fix: tramp stuff

--- modules/git/packages.el
+++ modules/git/packages.el
@@ -18,12 +18,22 @@
   :defer t
   :init
   :config
-  (setq tramp-use-ssh-controlmaster-options nil)
-  (setq tramp-default-method "ssh")
+
+  (setq tramp-verbose 0
+        tramp-chunksize 2000
+        tramp-use-ssh-controlmaster-options nil
+        tramp-default-method "ssh"
+        tramp-verbose 1
+        tramp-default-remote-shell "/bin/sh"
+        tramp-connection-local-default-shell-variables
+        '((shell-file-name . "/bin/bash")
+          (shell-command-switch . "-c")))
   (setq tramp-methods
         (seq-remove (lambda (m) (member (car m) '("gdrive" "davs" "dav")))
                     tramp-methods))
   (setq tramp-connection-properties
         (append tramp-connection-properties
                 '((nil "disable-ipv6" t)
-                  (nil "suppress-progress-reporter" t)))))
+                  (nil "suppress-progress-reporter" t))))
+  (setq vc-handled-backends '())
+  (setq vc-ignore-dir-regexp ".+"))
