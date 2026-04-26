1f86b66112e2a5b0428a1101fab607117b94cf83
rondDev <contact@rond.cc>
2026-04-27 00:01:55 +0200

    fix: move perspective

--- modules/default/config.el
+++ modules/default/config.el
@@ -71,23 +71,6 @@
         (setq completion-category-defaults nil)
         (setq completion-pcm-leading-wildcard t))
 
-(after! persp-projectile
-        (setq persp-mode-prefix-key "SPC.")
-        (persp-mode))
-
-(after! perspective
-        (defun persp-new (name)
-          "Return a perspective named NAME, or create a new one if missing.
-The new perspective will start with only an `initial-major-mode'
-buffer called \"*scratch* (NAME)\"."
-          (or (gethash name (perspectives-hash))
-              (make-persp :name name
-                (persp-reset-windows)))))
-
-(after! projectile-git-autofetch
-        (setopt projectile-git-autofetch-notify nil)
-        (setopt projectile-git-autofetch-interval 60)
-        (setopt projectile-git-autofetch-fetch-args '("--no-progress" "--prune" "--prune-tags")))
 
 (after! smartparens
         (require 'smartparens-config)) ;; add `smartparens-mode` to these hooks
