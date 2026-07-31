560a6e9c3105fb90318bc0b524b759dcf3387c8b
rondDev <contact@rond.cc>
2026-07-31 14:15:55 +0200

    fix: disable apheleia for ruby

--- modules/lang/ruby/config.el
+++ modules/lang/ruby/config.el
@@ -1,6 +1,15 @@
 ;; -*- lexical-binding: t; -*-
 (rassq-delete-all 'ruby-ts-mode auto-mode-alist)
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+
+;; (after! apheleia
+;;         (setf apheleia-formatters
+;;               (assq-delete-all 'prettier-ruby apheleia-formatters))
+
+;;         (push '(ruby-mode . rubocop) apheleia-mode-alist)
+;;         (push '(ruby-ts-mode . rubocop) apheleia-mode-alist)
+;;         (push '(rubocop . ("rubocop")) apheleia-formatters))
+
 (after! eglot
         (add-to-list 'eglot-server-programs
                      '(ruby-mode . ("ruby-lsp"))))
