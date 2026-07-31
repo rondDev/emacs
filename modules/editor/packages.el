20e79134305cad9eeb2ae977ea6913f3ed4fae56
rondDev <contact@rond.cc>
2026-07-31 14:12:48 +0200

    feat: add ghostel

--- modules/editor/packages.el
+++ modules/editor/packages.el
@@ -28,6 +28,50 @@
 
 (package! evil-multiedit)
 
+(package! ghostel
+  :bind (("C-x m" . ghostel)
+         :map ghostel-semi-char-mode-map
+         ("C-s"  . consult-line)
+         ("C-k"  . my/ghostel-send-C-k-and-kill)
+         ;; I'm used to go up/down the shell history with M-n/p from eshell
+         ;; Simulate this behavior in ghostel by sending C-p and C-n
+         ("M-p" . (lambda () (interactive) (ghostel-send-key "p" "ctrl")))
+         ("M-n" . (lambda () (interactive) (ghostel-send-key "n" "ctrl")))
+         :map project-prefix-map
+         ("m" . ghostel-project)
+         ("M" . ghostel-project-list-buffers))
+  :config
+  (defun my/ghostel-send-C-k-and-kill ()
+    "Send `C-k' to ghostel.
+Like normal Emacs `C-k'.  Kill to end of line and put content in kill-ring."
+    (interactive)
+    (kill-ring-save (point) (line-end-position))
+    (ghostel-send-key "k" "ctrl"))
+
+  (add-to-list 'project-switch-commands '(ghostel-project "Ghostel") t)
+  (add-to-list 'project-switch-commands '(ghostel-project-list-buffers "Ghostel buffers") t)
+  (add-to-list 'ghostel-eval-cmds '("magit-status-setup-buffer" magit-status-setup-buffer))
+  (setq ghostel-shell (cond
+                        ((f-executable-p "/usr/bin/nu") "/usr/bin/nu")
+                        ((f-executable-p "/usr/bin/zsh") "/usr/bin/zsh")
+                        (t "/usr/bin/bash"))))
+
+(package! evil-ghostel
+  :after (ghostel evil)
+  :hook (ghostel-mode . evil-ghostel-mode))
+(package! ghostel-eshell
+  :after (ghostel)
+         :ensure nil
+  :hook (eshell-load . ghostel-eshell-visual-command-mode))
+(package! ghostel-compile
+  :after (ghostel)
+         :ensure nil
+  :hook (after-init . ghostel-compile-global-mode))
+(package! ghostel-comint
+  :after (ghostel)
+         :ensure nil
+  :hook (after-init . ghostel-comint-global-mode))
+
 (package! helpful
   :commands helpful--read-symbol
   :init
