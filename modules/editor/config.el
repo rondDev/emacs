d584f95b9d6b5cd5be8880b9dd03223c3bd5253d
rondDev <contact@rond.cc>
2026-07-31 14:13:53 +0200

    feat: use home-manager folders for config find bind

--- modules/editor/config.el
+++ modules/editor/config.el
@@ -7,21 +7,49 @@
    "~/.config/neovim/init.lua"))
 
 ;; NOTE: Ensures it has a timestamp because nil will always be less
-(defun rond//file-modification-time (f)
+(defun rond//file-mod-time (f)
   (or (file-attribute-modification-time (file-attributes f)) '(0 0)))
 
 ;; NOTE: I feel like this is really ugly, but idk a better way to do it
+;; (defun rond/find-config-file ()
+;;   "Function to find config files for easy editing"
+;;   (interactive)
+;;   (find-file
+;;    (consult--read
+;;     (sort
+;;       (split-string (string-trim (shell-command-to-string "fd .  ~/.config/home-manager/config/ -d 1 -t d")))
+;;      (lambda (a b)
+;;        (time-less-p (rond//file-mod-time b) (rond//file-mod-time a))))
+
+;;     :prompt "Config file: " :sort nil :category 'file)))
+
+
+
 (defun rond/find-config-file ()
-  "Function to find config files for easy editing"
   (interactive)
-  (find-file
-   (consult--read
-    (sort
-     rond/config-files
-     (lambda (a b)
-       (time-less-p (rond//file-modification-time b) (rond//file-modification-time a))))
+  (let* ((base-dir (expand-file-name "~/.config/home-manager/config/"))
+         (candidates
+          (mapcar (lambda (path)
+                    (let* ((short (file-name-as-directory (file-name-nondirectory (directory-file-name path))))
+                           (padded-display (truncate-string-to-width short 25 nil ?\s)))
+                      (propertize short 'display padded-display)))
+           (sort (directory-files base-dir t "^[^.]" t)
+                      (lambda (a b)
+                        (time-less-p (rond//file-mod-time b)
+                                     (rond//file-mod-time a)))))))
     
-    :prompt "Config file: " :sort nil :category 'file)))
+    (minibuffer-with-setup-hook
+        (lambda () (setq default-directory base-dir))
+      
+      (let ((selection (consult--read
+                        candidates
+                        :prompt "Config file: "
+                        :sort nil
+                        :category 'file
+                        :annotate (lambda (cand)
+                                    (marginalia-annotate-file cand)))))
+        
+        (find-file (expand-file-name selection base-dir))))))
 
 (after! evil-multiedit
         (evil-multiedit-default-keybinds))
