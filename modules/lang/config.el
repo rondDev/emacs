ddbc8583ec2370d1805977ac27a34b0186806dfe
rondDev <contact@rond.cc>
2026-08-02 04:46:23 +0200

    fix: autoshutdown eglot

--- modules/lang/config.el
+++ modules/lang/config.el
@@ -91,14 +91,15 @@
     "i"  'elisp-index-search))
 
 (after! eglot
-  (add-hook 'eglot-managed-mode-hook
-    (lambda ()
-      ;; Show flymake diagnostics first.
-      (setq eldoc-documentation-functions
-        (cons #'flymake-eldoc-function
-          (remove #'flymake-eldoc-function eldoc-documentation-functions)))
-      ;; Show all eldoc feedback.
-      (setq eldoc-documentation-strategy #'eldoc-documentation-compose))))
+ (setq eglot-autoshutdown t)
+ (add-hook 'eglot-managed-mode-hook
+   (lambda ()
+     ;; Show flymake diagnostics first.
+     (setq eldoc-documentation-functions
+       (cons #'flymake-eldoc-function
+         (remove #'flymake-eldoc-function eldoc-documentation-functions)))
+     ;; Show all eldoc feedback.
+     (setq eldoc-documentation-strategy #'eldoc-documentation-compose))))
 
 
 
